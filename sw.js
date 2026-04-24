7c8bbfa68f8faadbe22fbb29c027e4962313dccf
Kingtutz <challe-1992@hotmail.com>
2026-04-24 21:29:45 +0200

    test

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'wc-betting-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/Css/shared.css',
+  '/Css/landing.css',
+  '/Css/account-modal.css',
+  '/Js/landing.js',
+  '/Js/firebase.js',
+  '/Js/i18n.js',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
