63b0d0725c2198b86c4d67d3489d3ceb2eac29d3
Kingtutz <challe-1992@hotmail.com>
2026-04-24 21:49:38 +0200

    made buttons bigger

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wc-betting-v1';
+const CACHE_NAME = 'wc-betting-v2'
 const ASSETS = [
   '/',
   '/index.html',
@@ -9,20 +9,37 @@ const ASSETS = [
   '/Js/firebase.js',
   '/Js/i18n.js',
   '/favicon.svg'
-];
+]
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS)
     })
-  );
-});
+  )
+  self.skipWaiting()
+})
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .filter(cacheName => cacheName !== CACHE_NAME)
+            .map(cacheName => caches.delete(cacheName))
+        )
+      )
+  )
+
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
     })
-  );
-});
+  )
+})
